5050b130f43944c33176a8290cc67d5050612f39
Ariel Silva <51482182+Alwexis@users.noreply.github.com>
2022-09-08 10:49:19 -0300

    Web Scrapping
    Web Scrapping para lo del Rut, hay que arreglar un bug, que no permite enviar petición.

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import fetch from "node-fetch";
+import * as cheerio from 'cheerio';
 
 @Component({
   selector: 'app-register',
@@ -36,5 +38,19 @@ export class RegisterPage implements OnInit {
       document.getElementsByClassName('patenteInput')[0].setAttribute("style", "display: none;");
     }
     */
+  }
+
+  async fetchData(url: string) {
+    const response = await fetch(url);
+    const data = await response.text();
+    return data;
+  }
+
+  async getPersonalInformationByRut(rut) {
+    console.log(rut)
+    const rawData = await this.fetchData("https://www.nombrerutyfirma.com/rut?term=" + rut);
+    // parsing the data
+    const data = cheerio.load(rawData);
+    console.log(data);
   }
 }
